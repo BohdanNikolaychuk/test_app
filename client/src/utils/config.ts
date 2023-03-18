feab7f1fe5144bc8886cd42a443e2c162ab14e6c
Bohdan <60944532+BohdanNikolaychuk@users.noreply.github.com>
2023-03-18 20:28:20 +0200

    Update config.ts

--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:8080'
+export const BASE_URL = 'https://treello-clone-test-app.onrender.com/
 
 export const deleteByID = (id: string, typeOfValue: string) =>
 	BASE_URL + `/${typeOfValue}/` + id
